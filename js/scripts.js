9779ef0019227644cab4f619115c924b2fba9de8
Chitroopa Manikkavasagam and Marcel Nogueira <chitroopa@>
2017-03-27 11:09:57 -0700

    use DRY to redo mad-lib javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,15 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+   var allId = ["person1", "person2","animal","exclamation","verb","noun","bird"];
 
-    var birdInput = $("input#bird").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+   allId.forEach(function(all){
+     var userInput = $("input#" + all).val();
+     alert(userInput);
+     $("." + all).text(userInput);
 
-    $(".bird").text(birdInput);
-
+   });
+  
     $("#story").show();
 
     event.preventDefault();
